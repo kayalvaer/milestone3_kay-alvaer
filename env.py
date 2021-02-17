56ef4d05989d8eca3ac554e6f100b1d33b43042d
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-02-17 01:07:27 +0100

    first commit

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "ThouHf7KW9")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "userStory_manager")
