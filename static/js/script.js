8e4990bf7bfeb5fe64d3572ca4197765e411ddec
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-02-18 12:04:24 +0100

    Changes to Nav and add bootstrap-materialize

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+/*
+    jQuery for Bootstrap-MaterializeCSS initialization
+*/
+
+$(function() {
+    $('#collapse-search').on('hidden.bs.collapse', function() {
+      $('#search').val('')
+    })
+  });
+
+
+
+
+
+
+
